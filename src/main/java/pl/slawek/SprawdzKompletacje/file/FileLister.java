a4945265c1c865785fbbed04517e4de46104a72c
Slawek <slapyt84@gmail.com>
2023-03-26 09:29:47 +0200

    Poprawa kodu i zmana nazwy na FileLister.java

---
+++ src/main/java/pl/slawek/SprawdzKompletacje/file/FileLister.java
@@ -0,0 +1,24 @@
+package pl.slawek.SprawdzKompletacje.file;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Data
+@Component
+public class FileLister {
+
+    private File directory = new File("C:\\Users\\slapy\\OneDrive\\Dokumenty\\Projekty\\SprawdzKompletacje\\");
+
+    private List<String> getExcelFileNames() {
+        return Arrays.stream(Objects.requireNonNull(directory.listFiles()))
+                .filter(file -> file.getName().endsWith(".xlsx"))
+                .map(File::getName)
+                .collect(Collectors.toList());
+    }
+}
