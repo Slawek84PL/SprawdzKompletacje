6af4040eca03533e791688901b7c9a2311d8af63
Slawek84PL <93817686+Slawek84PL@users.noreply.github.com>
2023-04-14 20:13:12 +0200

    Update ExcelWriterTest.java

--- src/test/java/pl/slawek/SprawdzKompletacje/file/ExcelWriterTest.java
+++ src/test/java/pl/slawek/SprawdzKompletacje/file/ExcelWriterTest.java
@@ -9,7 +9,7 @@ class ExcelWriterTest {
     ExcelWriter excelWriter;
     @Test
     void shouldWriteProductToExcelFile() throws IOException {
-        String file = "C:\\Users\\slapy\\OneDrive\\Dokumenty\\Projekty\\SprawdzKompletacje\\pliki\\zlecenie 12.xlsx";
+        String file = ".\\SprawdzKompletacje\\pliki\\zlecenie 12.xlsx";
         Product product = new Product("123", "nazwa", 10, 11);
 
         excelWriter.updateProduct(file,product);
