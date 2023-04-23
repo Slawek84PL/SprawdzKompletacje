c3f3f56cbb151cc7b65306c0fbda9b7a492e7824
Slawek <slapyt84@gmail.com>
2023-04-23 11:18:17 +0200

    adnotacje do obsługi DB

--- src/main/java/pl/slawek/SprawdzKompletacje/skan/Product.java
+++ src/main/java/pl/slawek/SprawdzKompletacje/skan/Product.java
@@ -1,17 +1,25 @@
 package pl.slawek.SprawdzKompletacje.skan;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
+
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@Column(name = "id", nullable = false)
+    private Long id;
 
     private String barcode;
     private String name;
     private int quantity;
     private int scannedQuantity;
+
 
 }
