7fd759087bd399a8be411e5c130bfd8cdf2880f0
Slawek <slapyt84@gmail.com>
2023-05-03 21:46:55 +0200

    poprawne zapisywanie do bazy. Okazało się, że H2 działa inaczej niż Azure SQL

--- src/main/java/pl/slawek/SprawdzKompletacje/entity/product/ProductService.java
+++ src/main/java/pl/slawek/SprawdzKompletacje/entity/product/ProductService.java
@@ -2,7 +2,10 @@ package pl.slawek.SprawdzKompletacje.entity.product;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import pl.slawek.SprawdzKompletacje.entity.order.OrderNumber;
+import pl.slawek.SprawdzKompletacje.entity.order.OrderRepository;
 import pl.slawek.SprawdzKompletacje.entity.product.scanned.ScannedPosition;
+import pl.slawek.SprawdzKompletacje.entity.product.scanned.ScannedPositionRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,24 +14,16 @@ import java.util.List;
 public class ProductService {
 
     private final ProductRepository productRepo;
+    private final ScannedPositionRepository positionRepository;
+    private final OrderRepository orderRepository;
 
-    public ProductService(final ProductRepository productRepo) {
+    public ProductService(final ProductRepository productRepo, final ScannedPositionRepository positionRepository, final OrderRepository orderRepository) {
         this.productRepo = productRepo;
+        this.positionRepository = positionRepository;
+        this.orderRepository = orderRepository;
     }
 
-    @Transactional
-    public void updatePositionOnProduct(final Product product, int scannedQuantity) {
 
-        ScannedPosition scannedPosition = new ScannedPosition();
-        scannedPosition.setScannedTime(LocalDateTime.now());
-        scannedPosition.setScannedQuantity(scannedQuantity);
-        scannedPosition.setProduct(product);
-
-        product.setScannedQuantity(product.getScannedQuantity() + scannedQuantity);
-        product.getScannedPositionList().add(scannedPosition);
-
-        productRepo.save(product);
-    }
 
     public List<Product> getProductByOrderId(long id) {
         return productRepo.findByOrderNumber_Id(id);
