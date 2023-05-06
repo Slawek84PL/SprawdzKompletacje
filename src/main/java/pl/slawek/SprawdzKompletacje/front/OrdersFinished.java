04116f5c875626edec398865f486f1d494cf18ef
Slawek <slapyt84@gmail.com>
2023-05-06 21:42:17 +0200

    dodałęm dwa gridy, które pokazują szczegóły zamówienia i ich realizację, oraz konkretne wpisy do DB ze szczegółami skanowania

--- src/main/java/pl/slawek/SprawdzKompletacje/front/OrdersFinished.java
+++ src/main/java/pl/slawek/SprawdzKompletacje/front/OrdersFinished.java
@@ -1,7 +1,13 @@
 package pl.slawek.SprawdzKompletacje.front;
 
+import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.data.provider.ListDataProvider;
+import com.vaadin.flow.data.renderer.LocalDateTimeRenderer;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,42 +15,86 @@ import pl.slawek.SprawdzKompletacje.entity.DataService;
 import pl.slawek.SprawdzKompletacje.entity.order.OrderNumber;
 import pl.slawek.SprawdzKompletacje.entity.order.OrderService;
 import pl.slawek.SprawdzKompletacje.entity.product.Product;
+import pl.slawek.SprawdzKompletacje.entity.product.scanned.ScannedPosition;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Route(value = "Zakończone", layout = MainView.class)
+@Route(value = "Zakonczone", layout = MainView.class)
 @PageTitle("Zakończone zamówienia")
-class OrdersFinished extends Div {
-    private final Grid<Product> orderNumberGrid = new Grid<>();
+class OrdersFinished extends VerticalLayout {
+    private final Grid<OrderNumber> orderGrid = new Grid<>(OrderNumber.class, false);
+    private final Grid<Product> productGrid = new Grid<>(Product.class,false);
+    private final Grid<ScannedPosition> detailsGrid = new Grid<>(ScannedPosition.class,false);
     private final DataService dataService;
     private final OrderService orderService;
     private List<Product> productList = new ArrayList<>();
-    private List<OrderNumber> orderNumberList = new ArrayList<>();
+    private List<OrderNumber> orderList = new ArrayList<>();
 
     @Autowired
     OrdersFinished(final DataService dataService, final OrderService orderService) {
         this.dataService = dataService;
         this.orderService = orderService;
         dataInitialize();
-        configureTreeGrid();
+        configureOrderGrid();
+        configureProductGrid();
+        configureDetailsGrid();
         addComponents();
     }
 
     private void dataInitialize() {
-        orderNumberList = orderService.findAllFinishedOrder();
-        System.out.println(orderNumberList);
+        orderList = orderService.findAllFinishedOrder();
+        for (OrderNumber o : orderList) {
+            System.out.println(o.getProducts());
+        }
+        System.out.println(orderList);
 
+    }
+
+    private void configureOrderGrid() {
+        orderGrid.addColumn(OrderNumber::getFileName).setHeader("Numer Zamówienia");
+        orderGrid.addColumn(new LocalDateTimeRenderer<>(OrderNumber::getImportDate, "yyyy-MM-dd HH:mm")).setHeader("Data importu").setWidth("100px");
+        orderGrid.addColumn(new LocalDateTimeRenderer<>(OrderNumber::getFinishedDate, "yyyy-MM-dd HH:mm")).setHeader("Data zakończenia");
+        orderGrid.setSelectionMode(Grid.SelectionMode.SINGLE);
+        formatMyGrid(orderGrid);
+        orderGrid.setVisible(true);
+        orderGrid.setItems(orderList);
+        orderGrid.addItemClickListener(e -> {
+            productList = e.getItem().getProducts();
+            productGrid.setItems(productList);
+            productGrid.setVisible(true);
+        });
+    }
+
+    private void configureProductGrid() {
+        productGrid.addColumn(Product::getName).setHeader("Nazwa productu");
+        productGrid.addColumn(Product::getBarcode).setHeader("Kod prodyctu");
+        productGrid.addColumn(Product::getQuantity).setHeader("Ilość zamówiona");
+        productGrid.addColumn(Product::getScannedQuantity).setHeader("Ilość zeskanowana");
+        productGrid.setSelectionMode(Grid.SelectionMode.SINGLE);
+        formatMyGrid(productGrid);
+        productGrid.addItemClickListener(e -> {
+            detailsGrid.setItems(e.getItem().getScannedPositionList());
+            detailsGrid.setVisible(true);
+        });
+    }
+
+    private void configureDetailsGrid() {
+        formatMyGrid(detailsGrid);
+        detailsGrid.setSelectionMode(Grid.SelectionMode.NONE);
+        detailsGrid.addColumn(new LocalDateTimeRenderer<>(ScannedPosition::getScannedTime, "yyyy-MM-dd HH:mm")).setHeader("Data skanowania");
+        detailsGrid.addColumn(ScannedPosition::getScannedQuantity).setHeader("Ilość zatwierdzonych sztuk");
+    }
+
+    private void formatMyGrid(Grid grid) {
+        grid.setVisible(false);
+        grid.setWidth(50, Unit.PERCENTAGE);
+        grid.addThemeVariants(GridVariant.LUMO_COMPACT);
     }
 
     private void addComponents() {
-        add(orderNumberGrid);
-    }
-
-    private void configureTreeGrid() {
-        orderNumberGrid.setItems((productList));
-        orderNumberGrid.addColumn(Product::getOrderNumber).setHeader("Numer Zamówienia");
-        orderNumberGrid.addColumn(Product::getName).setHeader("Pozycja Zamówienia");
-        orderNumberGrid.addColumn(Product::getScannedQuantity);
+//        Div divGrid = new Div();
+//        divGrid.add(productGrid,detailsGrid);
+        add(orderGrid, productGrid, detailsGrid);
     }
 }
