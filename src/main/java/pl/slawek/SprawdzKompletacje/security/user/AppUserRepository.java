2adce36b780eb6a811cc82d4b7872ea1db2a741d
Slawek <slapyt84@gmail.com>
2023-05-13 17:15:22 +0200

    repozytorium dla usera

---
+++ src/main/java/pl/slawek/SprawdzKompletacje/security/user/AppUserRepository.java
@@ -0,0 +1,9 @@
+package pl.slawek.SprawdzKompletacje.security.user;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppUserRepository extends CrudRepository<AppUser, Long> {
+    AppUser findByUsername(String username);
+}
