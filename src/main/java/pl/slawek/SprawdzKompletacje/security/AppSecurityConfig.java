3d9c532f3a7cd8fc5dfee65df25bb02b9f982e8d
Slawek <slapyt84@gmail.com>
2023-05-16 20:51:17 +0200

    refaktoryzacja

--- src/main/java/pl/slawek/SprawdzKompletacje/security/AppSecurityConfig.java
+++ src/main/java/pl/slawek/SprawdzKompletacje/security/AppSecurityConfig.java
@@ -1,7 +1,6 @@
 package pl.slawek.SprawdzKompletacje.security;
 
 import com.vaadin.flow.spring.security.VaadinWebSecurity;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -12,8 +11,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import pl.slawek.SprawdzKompletacje.front.LoginView;
 import pl.slawek.SprawdzKompletacje.security.user.AppUserDetailsService;
-import pl.slawek.SprawdzKompletacje.security.user.AppUserRepository;
-
 
 @Configuration
 @EnableWebSecurity
@@ -41,36 +38,10 @@ public class AppSecurityConfig extends VaadinWebSecurity {
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
         setLoginView(http, LoginView.class);
-//        System.out.println("w addUser");
-//        AppUser user = new AppUser();
-//        user.setUsername("slawek");
-//        user.setPassword(passwordEncoder().encode("pass"));
-//        user.setRole(Role.ADMIN);
-//        user.setEnabled(true);
-//        repository.save(user);
     }
 
     @Override
     protected void configure(final WebSecurity web) throws Exception {
         super.configure(web);
     }
-
-//    @Bean
-//    JdbcUserDetailsManager users(DataSource dataSource) {
-//        return new JdbcUserDetailsManager(dataSource);
-//    }
-
-//    @Bean
-//    public UserDetailsManager userDetailsManager() {
-//        UserDetails user = User.withUsername("user")
-//                .password("{noop}pass")
-//                .roles("USER")
-//                .build();
-//
-//        UserDetails admin = User.withUsername("admin")
-//                .password("{noop}pass")
-//                .roles("ADMIN")
-//                .build();
-//        return new InMemoryUserDetailsManager(user, admin);
-//    }
 }
