d88d93f2e1bf40bb2d8f52f172d3e401fc3eb00b
Slawek <slapyt84@gmail.com>
2023-05-29 22:09:12 +0200

    dodaje ADMINA do bazy danych

---
+++ src/main/java/pl/slawek/SprawdzKompletacje/entity/user/AdminUserInitializer.java
@@ -0,0 +1,31 @@
+package pl.slawek.SprawdzKompletacje.entity.user;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+class AdminUserInitializer implements CommandLineRunner {
+
+    private final AppUserService service;
+    private final String userPass;
+
+    AdminUserInitializer(final AppUserService service, @Value("${admin.pass}") final String userPass) {
+        this.service = service;
+        this.userPass = userPass;
+    }
+
+    @Override
+    public void run(final String... args) throws Exception {
+        if (!service.existByUsername()) {
+            AppUser user = new AppUser();
+            user.setUsername("ADMIN");
+            user.setFirsName("Sławomir");
+            user.setLastName("Pytlarz");
+            user.setRole(Role.ADMIN);
+            user.setPassword(userPass);
+            user.setEnabled(true);
+            service.saveFirstAdmin(user);
+        }
+    }
+}
