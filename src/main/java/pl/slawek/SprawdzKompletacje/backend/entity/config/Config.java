b8870510d884b4b4500be684ee5a617b73d85848
Slawek <slapyt84@gmail.com>
2023-06-04 16:00:10 +0200

    encja będzie przechowywać w bazie konfigurację odczytu pliku do weryfikacji kompletacji

---
+++ src/main/java/pl/slawek/SprawdzKompletacje/backend/entity/config/Config.java
@@ -0,0 +1,27 @@
+package pl.slawek.SprawdzKompletacje.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import pl.slawek.SprawdzKompletacje.backend.entity.user.AppUser;
+
+import java.util.Date;
+
+@Data
+@Entity
+class Config {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String viewName;
+
+    private int columnNumber;
+
+    private String columnNameHeader;
+
+    private Date createDate;
+
+    @OneToOne
+    private AppUser user;
+}
