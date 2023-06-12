33ea66a27c5d339b0ca4b28cf30a9643eac1d24a
Slawek <slapyt84@gmail.com>
2023-06-12 15:09:49 +0200

    przeniesienie kolumn odczytu do osobnej klasy ConfigHeader

--- src/main/java/pl/slawek/SprawdzKompletacje/backend/entity/config/ConfigColumnReader.java
+++ src/main/java/pl/slawek/SprawdzKompletacje/backend/entity/config/ConfigColumnReader.java
@@ -1,35 +1,30 @@
 package pl.slawek.SprawdzKompletacje.backend.entity.config;
 
-import lombok.Data;
 import org.apache.poi.ss.usermodel.*;
-import org.springframework.stereotype.Component;
-
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
-@Component
 public class ConfigColumnReader {
 
     private DataFormatter dataFormatter =  new DataFormatter();
 
-    public List<Config> readExcelFile(File file) throws IOException {
+    public List<ConfigHeader> readExcelFile(File file) throws IOException {
 
-        List<Config> configs = new ArrayList<>();
+        List<ConfigHeader> configs = new ArrayList<>();
 
         try (Workbook workbook = WorkbookFactory.create(file)) {
 
             Sheet sheet = workbook.getSheetAt(0);
             int firstRow = sheet.getFirstRowNum();
-            Row hederRow = sheet.getRow(firstRow);
+            Row headerRow = sheet.getRow(firstRow);
 
-            for (Cell column : hederRow) {
-                Config config = new Config();
-                config.setColumnNumber(column.getColumnIndex());
-                config.setColumnNameHeader(dataFormatter.formatCellValue(column));
+            for (Cell column : headerRow) {
+                ConfigHeader config = new ConfigHeader();
+                config.setColumnIndex(column.getColumnIndex());
+                config.setColumnName(dataFormatter.formatCellValue(column));
                 configs.add(config);
             }
         }
---
+++ src/main/java/pl/slawek/SprawdzKompletacje/backend/entity/config/ConfigHeader.java
@@ -0,0 +1,10 @@
+package pl.slawek.SprawdzKompletacje.backend.entity.config;
+
+import lombok.Data;
+
+@Data
+public
+class ConfigHeader {
+    private int columnIndex;
+    private String ColumnName;
+}
